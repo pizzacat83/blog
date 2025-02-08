4b59ccd6fc67f71460bce881bb655e96bb663337
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2025-02-08 22:07:58 +0900

    refactoring

--- generators/post.fsx
+++ generators/post.fsx
@@ -23,21 +23,21 @@ let websocketScript =
     """
 
 
-let layout (post: Postloader.Post) (content: Postloader.Content) =
-    let language = content.language
+let layout (post: Lib.LocalizedPost) =
+    let language = post.language
 
     let published = post.published.ToString("yyyy-MM-dd")
 
-    Lib.layout language content.title $"""
-<article>
+    Lib.layout language post.title $"""
+<article>   
 
 <header>
 <time datetime="{published}">{published}</time>
 <h1>
-    {content.title}
+    {post.title}
 </h1>
 </header>
-    {content.body}
+    {post.body}
 </article>
     """ ["/assets/post.css"]
 
@@ -50,25 +50,19 @@ let langCode = function
     | Postloader.English -> "en"
     | Postloader.Japanese -> "ja"
 
-let generatePost (post: Postloader.Post): Post list = 
-    post.contents
-    |> List.map (fun content ->
-        let html = layout post content
-        { language = content.language; html = html }
+let generate' (ctx : SiteContents) (language: Postloader.Language): (string * string) seq =
+    let posts =
+        Lib.getLocalizedPosts ctx language
+    
+    posts
+    |> Seq.map (fun post ->
+            let html = layout post
+            let filename = sprintf "%s/posts/%s/index.html" (langCode  post.language) (post.key |> fun (Postloader.PostKey k) -> k) in
+            (filename, html)
     )
 
 let generate (ctx : SiteContents) (projectRoot: string) (_): list<string * string> =
-    let posts =
-        ctx.TryGetValues<Postloader.Post> ()
-        |> Option.defaultValue Seq.empty
-    
-    posts
-      |> Seq.map (fun post ->
-            generatePost post     
-            |> List.map (fun p ->
-                let filename = sprintf "%s/posts/%s/index.html" (langCode  p.language) (post.key |> fun (Postloader.PostKey k) -> k) in
-                (filename, p.html)
-            )
-      )
+    Postloader.languages
+    |> List.map (generate' ctx)
     |> Seq.concat
     |> List.ofSeq
