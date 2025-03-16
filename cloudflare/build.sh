167fbb9d8c6c993a23ac4bacf3f6636eab36a873
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2025-03-16 23:49:31 +0900

    fix build command

--- cloudflare/build.sh
+++ cloudflare/build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+set -eux
+cd "$(dirname "$0")/.."
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
-./dotnet/dotnet --version
-./dotnet/dotnet tool restore
-./dotnet/dotnet paket restore
-./dotnet/dotnet run --configuration Release --project fornax/src/Fornax build
+./dotnet-install.sh -c 8.0 --install-dir "$HOME/.dotnet"
+export PATH="$PATH:$HOME/.dotnet"
+dotnet --version
+dotnet tool restore
+dotnet paket restore
+dotnet run --configuration Release --project fornax/src/Fornax build
